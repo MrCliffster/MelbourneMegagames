d220c3e4bd3977bc6cda35aa2800f265eda31722
Tristan Cliff <tristan.cliff@hotmail.com>
2020-04-28 17:16:08 +1000

    Initial commit

---
+++ MelbourneMegagames/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MelbourneMegagames.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required, Display(Name = "Date of game")]
+        public DateTime GameDate { get; set; }
+    }
+}
---
+++ MelbourneMegagames/Pages/Games/OurGames.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MelbourneMegagames.Pages.Games
+{
+    public class OurGamesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
