c96b623ab2fc007d3826279a3e4e8812fae57e4c
Tristan Cliff <tcliff@deloitte.com.au>
2020-06-29 09:13:28 +1000

    Added some game logic

--- MelbourneMegagames/Pages/Games/AddGame.cshtml.cs
+++ MelbourneMegagames/Pages/Games/AddGame.cshtml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MelbourneMegagames.Data;
+using MelbourneMegagames.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using MelbourneMegagames.Data;
-using MelbourneMegagames.Models;
+using System.Threading.Tasks;
 
 namespace MelbourneMegagames.Pages.Games
 {
---
+++ MelbourneMegagames/Pages/Games/EditGame.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using MelbourneMegagames.Data;
+using MelbourneMegagames.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MelbourneMegagames.Pages.Games
+{
+    public class EditGameModel : PageModel
+    {
+        private readonly MelbourneMegagamesContext _context;
+
+        public EditGameModel(MelbourneMegagamesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Game Game { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            return RedirectToPage("OurGames");
+        }
+
+    }
+}
