4567a68ff893178040186fcdc28c0308685da28a
Tristan Cliff <mrcliff59@gmail.com>
2021-10-14 19:07:45 +1100

    Leftover work

--- MelbourneMegagames/melbourne-megagames/components/FAQ.tsx
+++ MelbourneMegagames/melbourne-megagames/components/FAQ.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { FAQProps } from "../types/types";
+import styles from "../styles/FAQ.module.css"
 
 export default function FAQ({ question, answer }: FAQProps) {
+    const [showMe, setShowMe] = useState(false);
+    function toggle() {
+        setShowMe(!showMe);
+    }
     return (
         <div>
-            <h3>{question}</h3>
-            <p>{answer}</p>
+            <button onClick={toggle} className={styles.questionbutton}>{question}</button>
+            <div className={styles.collapsible + ' ' + (showMe ? styles.expanded : '')}>
+                <p>{answer}</p>
+            </div>
         </div>
     )
 }
