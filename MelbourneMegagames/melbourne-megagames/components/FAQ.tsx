c88af895e59e786b50e867a2e5709aa9b589e13e
Tristan Cliff <mrcliff59@gmail.com>
2021-10-27 14:45:53 +1100

    Added icons to FAQ

--- MelbourneMegagames/melbourne-megagames/components/FAQ.tsx
+++ MelbourneMegagames/melbourne-megagames/components/FAQ.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FAQProps } from "../types/types";
 import styles from "../styles/FAQ.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function FAQ({ question, answer }: FAQProps) {
     const [showMe, setShowMe] = useState(false);
@@ -9,7 +10,15 @@ export default function FAQ({ question, answer }: FAQProps) {
     }
     return (
         <div>
-            <button onClick={toggle} className={styles.questionbutton}>{question}</button>
+            <div>
+                <button onClick={toggle} className={styles.questionButton}>
+                    {question}
+                    <span className={styles.faqChevron}>
+                        <FontAwesomeIcon icon={showMe ? "chevron-down" : "chevron-left"} />
+                    </span>
+                </button>
+
+            </div>
             <div className={styles.collapsible + ' ' + (showMe ? styles.expanded : '')}>
                 <p>{answer}</p>
             </div>
--- MelbourneMegagames/melbourne-megagames/pages/_app.tsx
+++ MelbourneMegagames/melbourne-megagames/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronLeft, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faChevronDown, faChevronLeft)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
